498d7bc97d65458172cded3e45e868da4e36f460
thientantran <thientantm@gmail.com>
2023-07-17 21:35:03 +0700

    setup routers and layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,10 @@
 import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Dashboard from "./scenes/Dashboard";
+import Layout from "./scenes/Layout";
 import { themeSettings } from "./theme";
 
 function App() {
@@ -11,10 +14,17 @@ function App() {
   console.log(mode);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        App Here
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
