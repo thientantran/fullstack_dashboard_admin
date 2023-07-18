d90b4ef680fabd49275965eb0c98ee143b422127
thientantran <thientantm@gmail.com>
2023-07-18 20:55:41 +0700

    Call API using redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,18 +4,23 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.jsx";
 
+import { api } from "./api.js";
 import "./index.css";
-
 import globalSliceReducer from "./store.js";
 
+import { setupListeners } from "@reduxjs/toolkit/dist/query/index.js";
 import { Provider } from "react-redux";
 
 const store = configureStore({
   reducer: {
     global: globalSliceReducer,
+    [api.reducerPath]: api.reducer, // thêm reducer được tạo từ api slice
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+  //thêm api middleware để thêm các tính năng caching, polling, invalidation, rtk-query
 });
-
+setupListeners(store.dispatch);
+// optional nhưng bắt buộc nếu muốn dùng tính năng refetchOnFocus/ refetchOnReconnect
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
--- client/src/scenes/Layout.jsx
+++ client/src/scenes/Layout.jsx
@@ -2,12 +2,16 @@ import { Box, useMediaQuery } from "@mui/material";
 import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 
+import { useGetUserQuery } from "../api";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 
 export default function Layout() {
   const isNonMobile = useMediaQuery("(min-width: 600px)");
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const { data } = useGetUserQuery("63701cc1f03239c72c000187");
+  console.log("data query", data);
   return (
     <Box display={isNonMobile ? "flex" : "block"} width="100%" height="100%">
       <Sidebar
