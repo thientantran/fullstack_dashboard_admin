06ec5535e0be0aaf87b0a933b1ec85cf04f2a568
thientantran <thientantm@gmail.com>
2023-07-19 10:17:41 +0700

    binding products data

--- client/src/scenes/Products.jsx
+++ client/src/scenes/Products.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, useMediaQuery } from "@mui/material";
 import React from "react";
 
 import { useGetProductsQuery } from "../api";
@@ -7,14 +7,27 @@ import Product from "../components/Product";
 
 export default function Products() {
   const { data, isLoading } = useGetProductsQuery();
+  const isNonMobile = useMediaQuery("(min-width:1000px)");
   console.log(data);
   return (
     <Box m="1.5rem 2.5rem">
       <Header subtitle="See your list of products" title="PRODUCTS" />
-      <Box mt="20px">
-        <Product />
-        <Product />
-        <Product />
+      <Box
+        mt="20px"
+        display="grid"
+        gridTemplateColumns="repeat(4, minmax(0,1fr))"
+        justifyContent="space-between"
+        rowGap="20px"
+        columnGap="1.33%"
+        sx={{
+          "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+        }}
+      >
+        {data || !isLoading ? (
+          data.map((product) => <Product key={product._id} />)
+        ) : (
+          <div>...Loading</div>
+        )}
       </Box>
     </Box>
   );
