89081c3af6529166e4b6097922af9bebe645c8b1
thientantran <thientantm@gmail.com>
2023-07-21 15:46:31 +0700

    binding data admin by using data grid

--- client/src/scenes/Admin.jsx
+++ client/src/scenes/Admin.jsx
@@ -1,4 +1,5 @@
 import { Box, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 
 import { useGetAdminsQuery } from "../api";
@@ -7,10 +8,34 @@ import Header from "../components/Header";
 export default function Admin() {
   const theme = useTheme();
   const { data, isLoading } = useGetAdminsQuery();
-  console.log(data);
+
+  const columns = [
+    { field: "_id", headerName: "ID", flex: 1 },
+    { field: "name", headerName: "Name", flex: 0.5 },
+    { field: "email", headerName: "Email", flex: 1 },
+    {
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      flex: 0.5,
+      renderCell: (params) => {
+        return params.value.replace(/^(\d{3})(\d{3})(\d{4})/, "($1)$2-$3");
+      },
+    },
+    { field: "country", headerName: "Country", flex: 0.4 },
+    { field: "occupation", headerName: "Occupation", flex: 1 },
+    { field: "role", headerName: "Role", flex: 0.5 },
+  ];
   return (
     <Box m="1.5rem 2.5rem">
       <Header title="ADMINS" subtitle="Managing admins and list of admins" />
+      <Box mt="40px" height="75vh">
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={data || []}
+          columns={columns}
+        />
+      </Box>
     </Box>
   );
 }
