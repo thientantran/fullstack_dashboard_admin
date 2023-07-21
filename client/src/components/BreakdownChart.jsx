9aa778615dc836011c588dc14a74b892d216874f
thientantran <thientantm@gmail.com>
2023-07-21 14:25:58 +0700

    breakdownchart component, plot the chart

--- client/src/components/BreakdownChart.jsx
+++ client/src/components/BreakdownChart.jsx
@@ -1,5 +1,116 @@
+import { Box, useTheme } from "@mui/material";
+import { ResponsivePie } from "@nivo/pie";
 import React from "react";
 
+import { useGetSalesQuery } from "../api";
+
 export default function BreakdownChart() {
-  return <div>BreakdownChart</div>;
+  const theme = useTheme();
+  const { data, isLoading } = useGetSalesQuery();
+  if (!data || isLoading) return "Loading...";
+  const colors = [
+    theme.palette.secondary[500],
+    theme.palette.secondary[300],
+    theme.palette.secondary[300],
+    theme.palette.secondary[500],
+  ];
+
+  const formattedData = Object.entries(data.salesByCategory).map(
+    ([category, sales], i) => ({
+      id: category,
+      label: category,
+      value: sales,
+      color: colors[i],
+    })
+  );
+  return (
+    <Box
+      height="100%"
+      width={undefined}
+      minHeight={undefined}
+      minWidth={undefined}
+      position="relative"
+    >
+      <ResponsivePie
+        data={formattedData}
+        theme={{
+          axis: {
+            domain: {
+              line: {
+                stroke: theme.palette.secondary[200],
+              },
+            },
+            legend: {
+              text: {
+                fill: theme.palette.secondary[200],
+              },
+            },
+            ticks: {
+              line: {
+                stroke: theme.palette.secondary[200],
+                strokeWidth: 1,
+              },
+              text: {
+                fill: theme.palette.secondary[200],
+              },
+            },
+          },
+          legends: {
+            text: {
+              fill: theme.palette.secondary[200],
+            },
+          },
+          tooltip: {
+            container: {
+              color: theme.palette.primary.main,
+            },
+          },
+        }}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        borderWidth={1}
+        borderColor={{
+          from: "color",
+          modifiers: [["darker", 0.2]],
+        }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor="#333333"
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: "color" }}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+          from: "color",
+          modifiers: [["darker", 2]],
+        }}
+        legends={[
+          {
+            anchor: "bottom",
+            direction: "row",
+            justify: false,
+            translateX: 0,
+            translateY: 56,
+            itemsSpacing: 0,
+            itemWidth: 100,
+            itemHeight: 18,
+            itemTextColor: "#999",
+            itemDirection: "left-to-right",
+            itemOpacity: 1,
+            symbolSize: 18,
+            symbolShape: "circle",
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </Box>
+  );
 }
