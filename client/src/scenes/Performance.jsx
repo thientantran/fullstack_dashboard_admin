1e1ee021e90c9b13853e7a0f282fc23a6eb48833
thientantran <thientantm@gmail.com>
2023-07-22 14:32:45 +0700

    Binding data, using datagrid

--- client/src/scenes/Performance.jsx
+++ client/src/scenes/Performance.jsx
@@ -1,4 +1,5 @@
 import { Box } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 import { useSelector } from "react-redux";
 
@@ -8,13 +9,39 @@ import Header from "../components/Header";
 export default function Performance() {
   const userId = useSelector((state) => state.global.userId);
   const { data, isLoading } = useGetUserPerformanceQuery(userId);
-  console.log(data);
+
+  const columns = [
+    { field: "_id", headerName: "ID", flex: 1 },
+    { field: "userId", headerName: "User ID", flex: 1 },
+    { field: "createdAt", headerName: "Created At", flex: 1 },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sorttable: false,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
   return (
     <Box m="1.5rem 2.5rem">
       <Header
         title="PERFORMANCE"
         subtitle="Track your Affiliate Sales Performance Here"
       />
+      <Box mt="40px" height="75vh">
+        <DataGrid
+          loading={isLoading || !data}
+          getRowId={(row) => row._id}
+          rows={(data && data.sales) || []}
+          columns={columns}
+        />
+      </Box>
     </Box>
   );
 }
