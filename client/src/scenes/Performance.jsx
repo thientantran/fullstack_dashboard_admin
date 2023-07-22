ba13c8770d366c8c9a8efcd71e68566367e17253
thientantran <thientantm@gmail.com>
2023-07-22 14:33:54 +0700

    css for table, and custome the columns menu

--- client/src/scenes/Performance.jsx
+++ client/src/scenes/Performance.jsx
@@ -1,15 +1,16 @@
-import { Box } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 import { useSelector } from "react-redux";
 
 import { useGetUserPerformanceQuery } from "../api";
+import DataGridCustomColumnMenu from "../components/DataGridCustomColumnMenu";
 import Header from "../components/Header";
 
 export default function Performance() {
   const userId = useSelector((state) => state.global.userId);
   const { data, isLoading } = useGetUserPerformanceQuery(userId);
-
+  const theme = useTheme();
   const columns = [
     { field: "_id", headerName: "ID", flex: 1 },
     { field: "userId", headerName: "User ID", flex: 1 },
@@ -34,12 +35,40 @@ export default function Performance() {
         title="PERFORMANCE"
         subtitle="Track your Affiliate Sales Performance Here"
       />
-      <Box mt="40px" height="75vh">
+      <Box
+        mt="40px"
+        height="75vh"
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
         <DataGrid
           loading={isLoading || !data}
           getRowId={(row) => row._id}
           rows={(data && data.sales) || []}
           columns={columns}
+          slots={{ columnMenu: DataGridCustomColumnMenu }}
         />
       </Box>
     </Box>
