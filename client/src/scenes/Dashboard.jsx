2d46063b68c54cf35446e92a966e299e29071800
thientantran <thientantm@gmail.com>
2023-07-22 16:02:25 +0700

    router and fetch api for dashboad page

--- client/src/scenes/Dashboard.jsx
+++ client/src/scenes/Dashboard.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { Box } from "@mui/material";
 
+import { useGetDashboardQuery } from "../api";
+import Header from "../components/Header";
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  const { data, isLoading } = useGetDashboardQuery();
+  console.log(data);
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+    </Box>
+  );
 }
