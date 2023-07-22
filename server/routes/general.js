92f6e04356261f5d9d0f3cc6b879b86cd493efc1
thientantran <thientantm@gmail.com>
2023-07-22 15:52:15 +0700

    backend - api for dashboardstat

--- server/routes/general.js
+++ server/routes/general.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllUser, getUser } from "../controllers/general.js";
+import { getAllUser, getDashboardStats, getUser } from "../controllers/general.js";
 
 const generalRouter = express.Router()
 
 generalRouter.get("/admin/:id", getUser)
 generalRouter.get("/admin/", getAllUser)
+generalRouter.get("/dashboard", getDashboardStats);
 
 export default generalRouter;
