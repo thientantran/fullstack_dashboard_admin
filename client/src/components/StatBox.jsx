cdd40963faa4c17e057306c70040560a2fdcb133
thientantran <thientantm@gmail.com>
2023-07-22 23:20:44 +0700

    layout dashbor header and create statbox component

---
+++ client/src/components/StatBox.jsx
@@ -0,0 +1,46 @@
+import { Email } from "@mui/icons-material";
+import { Box, Typography, useTheme } from "@mui/material";
+import React from "react";
+
+import { default as FletBetween, default as FlexBetween } from "./FlexBetween";
+export default function StatBox() {
+  const theme = useTheme();
+
+  return (
+    <Box
+      gridColumn="span 2"
+      gridRow="span 1"
+      display="flex"
+      flexDirection="column"
+      justifyContent="space-between"
+      p="1.25rem 1rem"
+      flex="1 1 100%"
+      bgcolor={theme.palette.background.alt}
+      borderRadius="0.55rem"
+    >
+      <FletBetween>
+        <Typography variant="h6" sx={{ color: theme.palette.secondary[100] }}>
+          Title
+        </Typography>
+        <Email sx={{ color: theme.palette.secondary[300], fontSize: "26px" }} />
+      </FletBetween>
+      <Typography
+        variant="h3"
+        fontWeight="600"
+        sx={{ color: theme.palette.secondary[200] }}
+      >
+        value
+      </Typography>
+      <FlexBetween gap="1rem">
+        <Typography
+          variant="h5"
+          fontStyle="italic"
+          sx={{ color: theme.palette.secondary.light }}
+        >
+          Increase
+        </Typography>
+        <Typography>Description</Typography>
+      </FlexBetween>
+    </Box>
+  );
+}
--- client/src/scenes/Dashboard.jsx
+++ client/src/scenes/Dashboard.jsx
@@ -1,13 +1,35 @@
-import { Box } from "@mui/material";
+import { DownloadOutlined } from "@mui/icons-material";
+import { Box, Button, useTheme } from "@mui/material";
 
 import { useGetDashboardQuery } from "../api";
+import FlexBetween from "../components/FlexBetween";
 import Header from "../components/Header";
 export default function Dashboard() {
   const { data, isLoading } = useGetDashboardQuery();
-  console.log(data);
+  const theme = useTheme();
   return (
     <Box m="1.5rem 2.5rem">
-      <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      <FlexBetween>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+        <Box>
+          <Button
+            sx={{
+              backgroundColor: theme.palette.secondary.light,
+              color: theme.palette.background.alt,
+              fontSize: "14px",
+              fontWeight: "bold",
+              padding: "10px 20px",
+              "&:hover": {
+                backgroundColor: theme.palette.secondary.dark,
+                color: theme.palette.secondary.light,
+              },
+            }}
+          >
+            <DownloadOutlined sx={{ mr: "10px" }} />
+            Download reports
+          </Button>
+        </Box>
+      </FlexBetween>
     </Box>
   );
 }
